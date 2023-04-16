68ff9058a75e597078b053a9bf75248cc403814b
Andreas Baechtold <pandorasmusicbox@hotmail.com>
2023-04-16 22:12:31 +0200

    Fixed findings from testing.

--- src/show_index.rb
+++ src/show_index.rb
@@ -1,6 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/ruby
 
-require("./git_index.rb")
+
+require_relative('git_index')
 
 
 def parse(path)
@@ -16,7 +17,7 @@ def as_hex(str)
 end
 
 
-def print_general_info()
+def print_general_info(index)
   puts("Index File Version: #{index.header.version}")
   puts("Index Entries: #{index.header.nr_of_index_entries}, Extensions: #{index.extensions.size}")
   puts("File Hash:")
@@ -24,41 +25,46 @@ def print_general_info()
 end
 
 
-def print_index_entries()
+def print_index_entries(index)
   puts("Index Entries [#{index.header.nr_of_index_entries}]:")
   for e in index.index_entries
     puts("+ #{e.data.entry_path_name} [#{e.file_stats.file_size} Bytes]")
-    puts("#{as_hex(e.data.object_hash)}")
     f = e.data.flags
-    puts("assume valid: #{f.assume_valid_flag}, extended: #{f.extended_flag}, stage: #{f.stage_flags}")
+    puts("Assume valid: #{f.assume_valid_flag}, Extended: #{f.extended_flag}, Stage: #{f.stage_flags}")
+    puts("#{as_hex(e.data.object_hash)}")
   end
 end
 
 
 def print_treecache_ext(ext)
   tc = ext.data.tree_cache
-  puts("Tree Cache [#{tc.size}]:")
+  puts("Tree Cache Extension [#{tc.size}]:")
   for t in tc
     path = t.path.size == 0 ? "./" : t.path
     puts("+ #{path}")
-    puts("#{as_hex(t.object_hash)}")
-    puts("#entries: #{t.nr_of_covered_index_entries.to_i}, #subtrees: #{t.nr_of_subtrees.to_i}")
+    nr_entries = t.nr_of_covered_index_entries.to_i
+    puts("Entries: #{nr_entries}, Subtrees: #{t.nr_of_subtrees.to_i}")
+    if nr_entries > 0
+      puts("#{as_hex(t.object_hash)}")
+    else
+      puts("<Changed>")
+    end
   end
 end
 
 
 def print_resolve_undo_ext(ext)
   reuc = ext.data.resolve_undo
-  puts("Resolve-Undo [#{reuc.size}]:")
+  puts("Resolve-Undo Extension [#{reuc.size}]:")
   for rue in reuc
     path = rue.path.size == 0 ? "./" : rue.path
     puts("+ #{path}")
-    puts("Stage 1 Mode: [#{rue.stage1}]")
-    puts("Stage 1 Obj: [#{as_hex(rue.stage1_obj)}]")
-    puts("Stage 2 Mode: [#{rue.stage2}]")
-    puts("Stage 2 Obj: [#{as_hex(rue.stage2_obj)}]")
-    puts("Stage 3 Mode: [#{rue.stage3}]")
-    puts("Stage 3 Obj: [#{as_hex(rue.stage3_obj)}]")
+    puts("Stage 1 Mode: #{rue.stage1}")
+    puts("#{as_hex(rue.stage1_obj)}")
+    puts("Stage 2 Mode: #{rue.stage2}")
+    puts("#{as_hex(rue.stage2_obj)}")
+    puts("Stage 3 Mode: #{rue.stage3}")
+    puts("#{as_hex(rue.stage3_obj)}")
   end
 end
 
@@ -69,11 +75,16 @@ def print_ext(ext)
 end
 
 
-def print_extensions()
-  if index.extensions.size > 0
-    for e in index.extensions
+def print_extensions(index, args)
+  nr_of_exts = index.extensions.size
+  last_index = nr_of_exts - 1
+  if nr_of_exts == 0
+    puts("Extensions: None")
+  else
+    show_all = args.key?(:all)
+    for i in 0..last_index
+      e = index.extensions[i]
       id = e.extension_signature
-      show_all = args.key?(:all)
       if id == GitIndex::TreeCacheExt::ID
         print_treecache_ext(e)
       elsif id == GitIndex::ResolveUndoExt::ID and show_all
@@ -81,19 +92,20 @@ def print_extensions()
       elsif show_all
         print_ext(e)
       end
+      if i != last_index
+        puts('')
+      end
     end
-  else
-    puts("Extensions: None")
   end
 end
 
 
-def print_index_file(index)
+def print_index_file(index, args)
   print_general_info(index)
-  puts("")
+  puts('')
   print_index_entries(index)
-  puts("")
-  print_extensions(index)
+  puts('')
+  print_extensions(index, args)
 end
 
 
@@ -110,14 +122,15 @@ end
 
 def show_help_and_exit()
   help_msg = <<HELP
-  Usuage:  #{File.basename(__FILE__)} [--option[=value]]
+Usuage:  #{File.basename(__FILE__)} [--option[=value]]
   
-  Options:
-    --path={path}  Path to the Git index file.
-                   Default: #{DefaultPath}
-    --all          Show all extensions
-                   Default: Only tree cache is shown
-  HELP
+Options:
+  --path={path}  Path to the Git index file.
+                 Relative to working directory.
+                 Default: #{DefaultPath}
+  --all          Show all extensions
+                 Default: Only tree cache is shown
+HELP
   puts(help_msg)
   exit
 end
@@ -134,7 +147,7 @@ path = args.key?(:path) ? args[:path] : DefaultPath
 
 if File.exist?(path)
   index = parse(path)
-  print_index_file(path)
+  print_index_file(index, args)
 else
   puts("Index file not found at: #{path}")
 end
